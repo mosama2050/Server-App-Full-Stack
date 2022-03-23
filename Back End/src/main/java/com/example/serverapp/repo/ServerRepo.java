70af5d039e97c6603b20246653abb12ac84b17be
Mohamed Osama <mohamed.osama@arpuplus.com>
2022-03-23 14:54:04 +0200

    repo and service

---
+++ Back End/src/main/java/com/example/serverapp/repo/ServerRepo.java
@@ -0,0 +1,8 @@
+package com.example.serverapp.repo;
+
+import com.example.serverapp.model.Server;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServerRepo extends JpaRepository<Server, Long> {
+    Server findByIpAddress(String ipAddress);
+}
